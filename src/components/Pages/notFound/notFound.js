011b0a7773670f326ebd0d76b980ef6b5e166164
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-07-05 21:51:08 +0300

    Update notFound.js

--- src/components/Pages/notFound/notFound.js
+++ src/components/Pages/notFound/notFound.js
@@ -4,12 +4,11 @@ import style from './notFound.module.css';
 
 function NotFound() {
   const navigate = useNavigate();
-  const [seconds, setSeconds] = useState(3);
   useEffect(() => {
     setTimeout(() => {
       navigate('/', { replace: true });
     }, 1500);
-  }, [navigate, seconds]);
+  }, [navigate]);
 
   return (
     <div className={style.div}>
